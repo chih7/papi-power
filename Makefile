3ee927c5ac0c6c7c3c88f38bbf2370956bdc142d
chih <i@chih.me>
2017-03-04 16:01:36 +0800

    update

--- Makefile
+++ Makefile
@@ -7,11 +7,14 @@ FILE = rp_t2
 
 all:    clean $(FILE)
 
-$(FILE):	$(FILE).o
+$(FILE):	$(FILE).o load.o
 	$(CC) $(LFLAGS) -o $(FILE) $(FILE).o $(PAPI_LIBRARY)
 
 $(FILE).o:	$(FILE).cpp
 	$(CC) $(CFLAGS) -I$(PAPI_INCLUDE) -c $(FILE).cpp
 
+load.o:		load.cpp
+	$(CC) $(CFLAGS) -c load.cpp
+	
 clean:
 	rm -f *.o *~ $(FILE)
--- rp_t2.cpp
+++ rp_t2.cpp
@@ -29,54 +29,63 @@ static int all_select_preset_events[] = {
     PAPI_L2_ICM,
     PAPI_L1_TCM,
     PAPI_L2_TCM,
+    
     PAPI_L3_TCM,
     PAPI_CA_SNP,
     PAPI_CA_SHR,
     PAPI_CA_CLN,
     PAPI_CA_ITV,
     PAPI_L3_LDM,
+    
     PAPI_TLB_DM,
     PAPI_TLB_IM,
     PAPI_L1_LDM,
     PAPI_L1_STM,
     PAPI_L2_LDM,
     PAPI_L2_STM,
+    
     PAPI_PRF_DM,
     PAPI_MEM_WCY,
     PAPI_STL_ICY,
     PAPI_FUL_ICY,
     PAPI_STL_CCY,
     PAPI_FUL_CCY,
+    
     PAPI_BR_UCN,
     PAPI_BR_CN,
     PAPI_BR_TKN,
     PAPI_BR_NTK,
     PAPI_BR_MSP,
     PAPI_BR_PRC,
+    
     PAPI_TOT_INS,
     PAPI_LD_INS,
     PAPI_SR_INS,
     PAPI_BR_INS,
     PAPI_RES_STL,
     PAPI_TOT_CYC,
+    
     PAPI_LST_INS,
     PAPI_L2_DCA,
     PAPI_L3_DCA,
     PAPI_L2_DCR,
     PAPI_L3_DCR,
     PAPI_L2_DCW,
+    
     PAPI_L3_DCW,
     PAPI_L2_ICH,
     PAPI_L2_ICA,
     PAPI_L3_ICA,
     PAPI_L2_ICR,
     PAPI_L3_ICR,
+    
     PAPI_L2_TCA,
     PAPI_L3_TCA,
     PAPI_L2_TCR,
     PAPI_L3_TCR,
     PAPI_L2_TCW,
     PAPI_L3_TCW,
+    
     PAPI_SP_OPS,
     PAPI_DP_OPS,
     PAPI_VEC_SP,
@@ -92,54 +101,63 @@ static char *all_select_preset_events_name[] = {
     "PAPI_L2_ICM",
     "PAPI_L1_TCM",
     "PAPI_L2_TCM",
+    
     "PAPI_L3_TCM",
     "PAPI_CA_SNP",
     "PAPI_CA_SHR",
     "PAPI_CA_CLN",
     "PAPI_CA_ITV",
     "PAPI_L3_LDM",
+    
     "PAPI_TLB_DM",
     "PAPI_TLB_IM",
     "PAPI_L1_LDM",
     "PAPI_L1_STM",
     "PAPI_L2_LDM",
     "PAPI_L2_STM",
+    
     "PAPI_PRF_DM",
     "PAPI_MEM_WCY",
     "PAPI_STL_ICY",
     "PAPI_FUL_ICY",
     "PAPI_STL_CCY",
     "PAPI_FUL_CCY",
+    
     "PAPI_BR_UCN",
     "PAPI_BR_CN",
     "PAPI_BR_TKN",
     "PAPI_BR_NTK",
     "PAPI_BR_MSP",
     "PAPI_BR_PRC",
+    
     "PAPI_TOT_INS",
     "PAPI_LD_INS",
     "PAPI_SR_INS",
     "PAPI_BR_INS",
     "PAPI_RES_STL",
     "PAPI_TOT_CYC",
+    
     "PAPI_LST_INS",
     "PAPI_L2_DCA",
     "PAPI_L3_DCA",
     "PAPI_L2_DCR",
     "PAPI_L3_DCR",
     "PAPI_L2_DCW",
+    
     "PAPI_L3_DCW",
     "PAPI_L2_ICH",
     "PAPI_L2_ICA",
     "PAPI_L3_ICA",
     "PAPI_L2_ICR",
     "PAPI_L3_ICR",
+    
     "PAPI_L2_TCA",
     "PAPI_L3_TCA",
     "PAPI_L2_TCR",
     "PAPI_L3_TCR",
     "PAPI_L2_TCW",
     "PAPI_L3_TCW",
+    
     "PAPI_SP_OPS",
     "PAPI_DP_OPS",
     "PAPI_VEC_SP",
@@ -148,12 +166,19 @@ static char *all_select_preset_events_name[] = {
 };
 
 static int select_preset_events[] = {
-    PAPI_SP_OPS,
-    PAPI_DP_OPS,
+
+
+
+    PAPI_FUL_ICY,
+    PAPI_FUL_CCY
+    
 };
+
 static char *select_preset_events_name[] = {
-    "PAPI_SP_OPS",
-    "PAPI_DP_OPS",
+
+
+    "PAPI_FUL_ICY",
+    "PAPI_FUL_CCY"
 };
 
 static int  EVENTS_NUM = sizeof(select_preset_events)/sizeof(select_preset_events[0]);
